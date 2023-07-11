4b33f057fa884ad40ceabe88ec864240923ef619
Maks <76596127+NoBugJustFeature@users.noreply.github.com>
2023-07-11 14:03:05 +0300

    Update after_clone.bat

--- after_clone.bat
+++ after_clone.bat
@@ -1,4 +1,12 @@
+@echo off
+
+echo Creating virtual environment...
 python -m venv env
-env\Scripts\activate.bat
+
+echo Installing dependencies...
 pip install -r requirements.txt
+
+echo Creating .env file for environment variables...
 type .env.example > .env
+
+echo Done!
